15e4e6cfa18326b06f482dc4b2d05e75a42dd443
Bryant Suiskens <r0739290@student.thomasmore.be>
2023-02-07 19:51:43 +0100

    excercise finished

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,39 @@
+<template>
+   <div   class="menu-item">
+          <img
+            class="menu-item__image"
+            :src="source"
+            :alt="alt"
+          />
+          <div>
+            <h3>{{ name }}</h3>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <div>
+              <label for="add-item-quantity"
+                >Quantity: {{ quantity }}</label
+              >
+              <input
+                v-model.number="quantity"
+                id="add-item-quantity"
+                type="number"
+              />
+              <button @click="addToCart(quantity)">
+                Add to Shopping Cart
+              </button>
+            </div>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: ['source', 'alt', 'name', 'inStock', 'quantity', 'addToCart']
+}
+</script>
+
+<style>
+
+
+</style>
